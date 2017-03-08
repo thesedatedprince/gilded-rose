98c82742f76edeb31bc09ecd1ab1c7d847c15cab
Matthew Perry <thesedatedprince@gmail.com>
2017-03-08 10:26:07 +0000

    finished basic update flow

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,20 +1,41 @@
 require 'item.rb'
 
 class GildedRose
+  attr_reader :items
 
   def initialize()
     @items = {}
   end
 
   def update_quality(item)
-    degrade_rate = @items[item.name.to_sym][0]
-    item.quality -= degrade_rate
+    quality_speed_update(item)
+    degrade_rate = quality_calc(item, @items[item.name.to_sym][0])
+    item.quality += degrade_rate
   end
 
-
-  def rules_generator(item, initial_quality_speed = 1, quality_change = 1, day_change = 0)
-    @items[item.name.to_sym] = [initial_quality_speed, day_change]
+  def add_item(item, quality_change_rate = -1)
+    @items[item.name.to_sym] =[quality_change_rate]
   end
 
+  def add_rule(item, speed_change_day, new_speed)
+    @items[item.name.to_sym].push [speed_change_day, new_speed]
+  end
 
+  def quality_speed_update(item)
+    update_speed(item) if change_date? (item)
+  end
+
+  private
+
+  def change_date? (item)
+    item.sell_in == @items[item.name.to_sym][1][0]
+  end
+
+  def update_speed (item)
+    @items[item.name.to_sym][0] = @items[item.name.to_sym][1][1]
+  end
+
+  def quality_calc (item, degrade_rate)
+    item.quality + degrade_rate > 0 ? degrade_rate : -item.quality
+  end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -12,31 +12,37 @@ describe GildedRose do
       end
 
       it "does not change the name" do
-        items = Item.new("foo", 0, 0)
-        @gilded_rose.rules_generator(items, 1, 10)
-        @gilded_rose.update_quality(items)
-        expect(items.name).to eq "foo"
+        item = Item.new("foo", 0, 0)
+        @gilded_rose.add_item(item)
+        @gilded_rose.add_rule(item, 7, 2)
+        @gilded_rose.update_quality(item)
+        expect(item.name).to eq "foo"
       end
 
       it "reduces quality of goods as they approach sell-by date" do
-        items =Item.new("foo", 7, 10)
-        @gilded_rose.rules_generator(items, 1, 10)
-        @gilded_rose.update_quality(items)
-        expect(items.quality).to eq 9
+        item =Item.new("foo", 7, 10)
+        @gilded_rose.add_item(item)
+        @gilded_rose.add_rule(item, 0, 2)
+        @gilded_rose.update_quality(item)
+        expect(item.quality).to eq 9
       end
 
 
-      # it "Reduces quality of goods at 2x speed after sell-by date has passed" do
-      #   items = Item.new("foo", 0, 10)
-      #   GildedRose.new().update_quality(items)
-      #   expect(items.quality).to eq 8
-      # end
+      it "Reduces quality of goods at 2x speed after sell-by date has passed" do
+        item = Item.new("foo", 0, 10)
+        @gilded_rose.add_item(item)
+        @gilded_rose.add_rule(item, 0, -2)
+        @gilded_rose.update_quality(item)
+        expect(item.quality).to eq 8
+      end
 
-      # it "never allows quality of item to dip below 0" do
-      #   items =[Item.new("foo", 0, 0)]
-      #   GildedRose.new(items).update_quality()
-      #   expect(items[0].quality).to eq 0
-      # end
+      it "never allows quality of item to dip below 0" do
+        item =Item.new("foo", 0, 0)
+        @gilded_rose.add_item(item)
+        @gilded_rose.add_rule(item, 0, -2)
+        @gilded_rose.update_quality(item)
+        expect(item.quality).to eq 0
+      end
 
   end
 
@@ -79,5 +85,55 @@ describe GildedRose do
     #     expect(items[0].quality).to eq 0
     #   end
     # end
+  end
+
+  describe "#add_item" do
+    before do
+      @gilded_rose = GildedRose.new
+    end
+    it 'adds a new item to the items hash - basic quality speed of 1' do
+        item =Item.new("foo", 7, 10)
+        @gilded_rose.add_item(item)
+        @gilded_rose.add_rule(item, 7, 2)
+        expect(@gilded_rose.items).to eq({:foo => [-1, [7,2]]})
+    end
+    it 'can instantiate with different quality change speeds' do
+        item =Item.new("foo", 7, 10)
+        @gilded_rose.add_item(item, 2)
+        @gilded_rose.add_rule(item, 0, 3)
+        expect(@gilded_rose.items).to eq({:foo => [2, [0,3]]})
+    end
+  end
+
+  describe "add_rule" do
+    before do
+      @gilded_rose = GildedRose.new
+    end
+    it "associates a rule set to each item" do
+      item =Item.new("foo", 7, 10)
+      @gilded_rose.add_item(item)
+      @gilded_rose.add_rule(item, 7, 2)
+      expect(@gilded_rose.items).to eq ({:foo => [-1, [7, 2]]})
+    end
+    it "can take multiple rule sets" do
+      item =Item.new("foo", 7, 10)
+      @gilded_rose.add_item(item)
+      @gilded_rose.add_rule(item, 7, 2)
+      @gilded_rose.add_rule(item, 3, 5)
+      expect(@gilded_rose.items).to eq ({:foo => [-1, [7, 2],[3, 5]]})
+    end
+  end
+
+  describe "quality_speed_update" do
+    before do
+      @gilded_rose = GildedRose.new
+    end
+    it "updates the degrade rate when it hits a specified day" do
+      item =Item.new("foo", 7, 10)
+      @gilded_rose.add_item(item)
+      @gilded_rose.add_rule(item, 7, -2)
+      @gilded_rose.quality_speed_update(item)
+      expect(@gilded_rose.items).to eq ({:foo => [-2, [7, -2]]})
+    end
   end
 end
